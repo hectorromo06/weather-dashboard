d410d143861395a21fadf5dd00cfe55a63e5b050
Hector Romo <hectorommmo@gmail.com>
2022-05-01 13:57:18 -0700

    page refresh issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
+var lat;
+var lon;
 var apiKey = '19dc82681f3d9f94a404d90b4c15201e';
-var apiCall = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=';
+var apiCall = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
+
+$('.submit-btn').click(function (event) {
+    event.preventDefault();
+    console.log("🚀 ~ file: script.js ~ line 7 ~ event", event);
+});
