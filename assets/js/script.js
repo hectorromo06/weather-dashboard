4414d66fcdab450ce168de3a7ba2feb90b1a64d8
Hector Romo <hectorommmo@gmail.com>
2022-05-01 18:07:25 -0700

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var apiKey = '19dc82681f3d9f94a404d90b4c15201e';
 var citiesSearched = [];
 
-
+// event listener for submit button
 $('#submit-btn').click(function (event) {
     event.preventDefault();
     var cityName = event.currentTarget.parentElement[0].value;
@@ -14,14 +14,15 @@ $('#submit-btn').click(function (event) {
 function getApi(city) {
     var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&appid=' + apiKey;
 
+    // checks if localStorage exists
     var temp = localStorage.getItem('citiesSearched');
     var exists = false;
 
+    // if localStorage does exist, make sure cityName is not duplicated
     if (temp != null) {
         citiesSearched = JSON.parse(temp);
 
         for (var i = 0; i < citiesSearched.length; i++) {
-            console.log(i);
             if (citiesSearched[i].cityName == city) {
                 exists = true;
                 break;
@@ -31,7 +32,7 @@ function getApi(city) {
         }
     }
 
-    console.log(exists);
+    // if cityName doesn't exist in array, adds it, creates localStorage
     if (exists == false) {
         var occurence = {
             cityName: city
@@ -52,6 +53,7 @@ function getApi(city) {
         });
 }
 
+// API call for current conditions
 function forecast(lat, lon) {
     var apiCall = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial&exclude=hourly,daily,minutely';
 
@@ -64,6 +66,7 @@ function forecast(lat, lon) {
         })
 }
 
+// API call for future conditions
 function fiveDay(lat, lon) {
     var apiCall = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey + '&units=imperial&cnt=5';
 
